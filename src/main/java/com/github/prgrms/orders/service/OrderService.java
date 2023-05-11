7acdefddc05832f1ee782036fcdb270dcd5865c0
kksks3192@gmail.com <kksks3192@gmail.com>
2023-05-11 10:55:44 +0900

    OrderService

--- src/main/java/com/github/prgrms/orders/service/OrderService.java
+++ src/main/java/com/github/prgrms/orders/service/OrderService.java
@@ -1,4 +1,30 @@
-package com.github.prgrms.orders.domain;
+package com.github.prgrms.orders.service;
 
+import com.github.prgrms.orders.dao.OrderRepository;
+import com.github.prgrms.orders.dao.ReviewRepository;
+import com.github.prgrms.orders.dto.ReviewDto;
+import com.github.prgrms.products.service.ProductService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class OrderService {
+
+    private final ProductService productservice;
+    private final ReviewRepository reviewrepository;
+    private final OrderRepository orderrepository;
+
+
+    public OrderService(ProductService productservice, ReviewRepository reviewRepository, OrderRepository orderRepository) {
+        this.productservice = productservice;
+        this.reviewrepository = reviewRepository;
+        this.orderrepository = orderRepository;
+    }
+
+    @Transactional
+    public ReviewDto review(Long orderId, String content){
+
+
+    }
 }
+
