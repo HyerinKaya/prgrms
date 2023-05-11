fef6e254eb9a7329876b6d68dc81f8b487a732e6
kksks3192@gmail.com <kksks3192@gmail.com>
2023-05-11 10:54:46 +0900

    order dto

--- src/main/java/com/github/prgrms/orders/dto/OrderDto.java
+++ src/main/java/com/github/prgrms/orders/dto/OrderDto.java
@@ -6,11 +6,9 @@ import com.github.prgrms.orders.constant.State;
 
 import java.time.LocalDateTime;
 
-public class OrderDto {
+import static org.springframework.beans.BeanUtils.copyProperties;
 
-    //seq, productId, reviewId, review, state
-    //requestMessage, rejectMessage, completedAt
-    //rejectedAt, createAt
+public class OrderDto {
 
     private Long seq;
 
@@ -20,7 +18,7 @@ public class OrderDto {
 
     private Long reviewId;
 
-    private Review review;
+    private ReviewDto review;
 
     private State state;
 
@@ -35,7 +33,95 @@ public class OrderDto {
     private LocalDateTime createAt;
 
     public OrderDto(Order source){
-        copyPro
+        copyProperties(source, this, "userId", "review");
+        review = source.getReview() == null ? null : new ReviewDto(source.getReview());
     }
 
+    public Long getSeq() {
+        return seq;
+    }
+
+    public void setSeq(Long seq) {
+        this.seq = seq;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(Long reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public ReviewDto getReview() {
+        return review;
+    }
+
+    public void setReview(ReviewDto review) {
+        this.review = review;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public String getRequestMessage() {
+        return requestMessage;
+    }
+
+    public void setRequestMessage(String requestMessage) {
+        this.requestMessage = requestMessage;
+    }
+
+    public String getRejectMessage() {
+        return rejectMessage;
+    }
+
+    public void setRejectMessage(String rejectMessage) {
+        this.rejectMessage = rejectMessage;
+    }
+
+    public LocalDateTime getCompletedAt() {
+        return completedAt;
+    }
+
+    public void setCompletedAt(LocalDateTime completedAt) {
+        this.completedAt = completedAt;
+    }
+
+    public LocalDateTime getRejectedAt() {
+        return rejectedAt;
+    }
+
+    public void setRejectedAt(LocalDateTime rejectedAt) {
+        this.rejectedAt = rejectedAt;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
 }
--- src/main/java/com/github/prgrms/orders/dto/ReviewDto.java
+++ src/main/java/com/github/prgrms/orders/dto/ReviewDto.java
@@ -1,4 +1,55 @@
 package com.github.prgrms.orders.dto;
 
+import com.github.prgrms.orders.domain.Review;
+
+import java.time.LocalDateTime;
+
 public class ReviewDto {
+
+    private Long seq;
+
+    private Long productId;
+
+    private String Content;
+
+    private LocalDateTime createAt;
+
+    public ReviewDto(Review source) {
+        this.seq = source.getSeq();
+        this.productId = source.getProductId();
+        this.Content = source.getContent();
+        this.createAt = source.getCreateAt();
+    }
+
+    public Long getSeq() {
+        return seq;
+    }
+
+    public void setSeq(Long seq) {
+        this.seq = seq;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
 }
